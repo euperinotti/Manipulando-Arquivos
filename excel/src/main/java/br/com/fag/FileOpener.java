025c1e009c6dac17b82af586180c3912042644d0
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-09-29 11:26:35 -0300

    feat: insert concurso id

--- excel/src/main/java/br/com/fag/FileOpener.java
+++ excel/src/main/java/br/com/fag/FileOpener.java
@@ -1,47 +1,60 @@
 package br.com.fag;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class FileOpener {
 
-  private static final String filePath = "/home/euperinotti/Documents/Github/Manipulando-Arquivos/excel/src/main/java/br/com/fag/data/Mega-Sena.xlsx";
-  private static final String fileName = "src/main/java/resources/Mega-Sena.xlsx";
-
   public static void main(String[] args) {
-    // List<Aposta> listaApostas = new ArrayList<Aposta>();
+    List<Aposta> listaApostas = new ArrayList<Aposta>();
 
     try {
-      // FileInputStream planilha = new FileInputStream(new
-      // File(FileOpener.fileName));
       InputStream planilha = new ResourceLoader().run();
       XSSFWorkbook workbook = new XSSFWorkbook(planilha);
       XSSFSheet sheetApostas = workbook.getSheetAt(0);
 
       Iterator<Row> rowIterator = sheetApostas.iterator();
 
-      while (rowIterator.hasNext()) {
-        Row row = rowIterator.next();
-        Iterator<Cell> cellIterator = row.cellIterator();
-        Aposta aposta = new Aposta();
+      boolean proximo = rowIterator.hasNext();
 
-        while (cellIterator.hasNext()) {
-          Cell cell = cellIterator.next();
-          System.out.println(cell.toString());
+      while (proximo) {
+        Row row = rowIterator.next();
+
+        if (row.getRowNum() > 0 && row.getRowNum() < 2) {
+          System.out.println("Linha " + row.getRowNum() + ":");
+          Iterator<Cell> cellIterator = row.cellIterator();
+          Aposta aposta = new Aposta();
+
+          while (cellIterator.hasNext()) {
+            Cell cell = cellIterator.next();
+            switch (cell.getColumnIndex()) {
+              case 0:
+                aposta.setConcurso((int) cell.getNumericCellValue());
+                break;
+              case 1:
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+                String date = cell.toString();
+                LocalDate localDate = LocalDate.parse(date, formatter);
+                System.out.println("data -> " + formatter.format(localDate));
+                break;
+            }
+            System.out.println(cell.toString());
+          }
+          proximo = false;
         }
+
       }
+
+      workbook.close();
 
     } catch (Exception e) {
       System.out.println("Erro: " + e);
