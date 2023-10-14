587acb8ab8a502a62ac77164590f3172649d9459
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-10-14 15:14:54 -0300

    test: ensure a new file is written

--- manipulando-arquivos/src/test/java/br/com/fag/services/FileWriterServiceTest.java
+++ manipulando-arquivos/src/test/java/br/com/fag/services/FileWriterServiceTest.java
@@ -4,14 +4,23 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.fag.infra.ResourceLoader;
 
 public class FileWriterServiceTest {
 
+  private FileWriterService sut;
+
+  @BeforeEach
+  void setService() {
+    sut = new FileWriterService();
+  }
+
   @Test
   void shouldWriteANewFile() throws IOException {
+    sut.start();
     ClassLoader classLoader = ResourceLoader.class.getClassLoader();
     InputStream inputStream = classLoader.getResourceAsStream("resultado.txt");
 
