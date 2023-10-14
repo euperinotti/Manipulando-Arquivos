79a5d3d5bc521bb868cd4f4dd13ed78d54051faf
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-10-14 13:13:09 -0300

    test: filter string test

--- manipulando-arquivos/src/test/java/br/com/fag/utils/FilterStringTest.java
+++ manipulando-arquivos/src/test/java/br/com/fag/utils/FilterStringTest.java
@@ -1,9 +1,15 @@
 package br.com.fag.utils;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FilterStringTest {
   
   @Test
-  void shouldReturnAStringWithoutDotsCommasAnd
+  void shouldReturnAStringWithOnlyNumbers(){
+    String price = "R$ 6.000,00";
+    String result = FilterString.sanitize(price);
+
+    Assertions.assertEquals("600000", result);
+  }
 }
