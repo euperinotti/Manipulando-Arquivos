c9da1b6978d6e0d3f8621f08f23986a59c8a5116
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-10-17 12:45:41 -0300

    test: quantas vezes cada numero foi sorteado

---
+++ manipulando-arquivos/src/test/java/br/com/fag/domain/usecases/QuantasVezesCadaNumeroFoiSorteadoTest.java
@@ -0,0 +1,43 @@
+package br.com.fag.domain.usecases;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.poi.hpsf.Array;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.fag.domain.entities.Aposta;
+import br.com.fag.domain.entities.Rateio;
+import br.com.fag.domain.entities.Resultado;
+
+public class QuantasVezesCadaNumeroFoiSorteadoTest {
+  
+  private QuantasVezesCadaNumeroFoiSorteado sut;
+  private Aposta aposta;
+
+  @BeforeEach
+  void setSut() {
+    aposta = new Aposta();
+    ArrayList<Integer> bolas = new ArrayList<>();
+    bolas.add(1);
+    aposta.setBolasSorteadas(bolas);
+    Resultado.quantasVezesCadaNumeroFoiSorteado = new HashMap<>();
+    sut = new QuantasVezesCadaNumeroFoiSorteado();
+  }
+  
+  @Test
+  void shouldIncrementVariable() {
+    sut.run(aposta);
+
+    Assertions.assertEquals(1, Resultado.quantasVezesCadaNumeroFoiSorteado.get(1));
+  }
+
+  @Test
+  void shouldNotIncrementVariable() {
+    sut.run(aposta);
+
+    Assertions.assertEquals(null, Resultado.quantasVezesCadaNumeroFoiSorteado.get(2));
+  }
+}
